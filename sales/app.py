13624dfd9ed3a5a73b5e4935673aae8ef9361a99
Prince Mfinanga <noahprince2@icloud.com>
2025-01-25 18:57:31 +0300

    Changed the Readme file

--- sales/app.py
+++ sales/app.py
@@ -12,12 +12,13 @@ ui.page_opts(title="Sales Dashboard - video 1 of 5", fillable=True)
 
 ui.input_numeric("n", label="Number of products to show", value=5, min=0, max=20)
 
+
 @reactive.calc
 def dat():
     infile = Path(__file__).parent / "data" / "sales.csv"
     df = pd.read_csv(infile)
-    df['order_date'] = pd.to_datetime(df['order_date'])
-    df['month'] = df['order_date'].dt.month_name()
+    df["order_date"] = pd.to_datetime(df["order_date"])
+    df["month"] = df["order_date"].dt.month_name()
     return df
 
 
@@ -26,26 +27,45 @@ def dat():
 #     df = dat()
 #     top_sales = df.groupby("product")['quantity_ordered'].sum().nlargest(input.n()).reset_index()
 #     return px.bar(top_sales, x = 'product', y = 'quantity_ordered', title = f'Top {input.n()} products by quantity sold')
-    
+
 ui.input_selectize(
     "city",
     "Select a City",
-    ['Dallas (TX)', 'Boston (MA)', 'Los Angeles (CA)', 'San Francisco (CA)', 'Seattle (WA)', 'Atlanta (GA)', 'New York City (NY)', 'Portland (OR)', 'Austin (TX)', 'Portland (ME)'],
-    multiple=True
+    [
+        "Dallas (TX)",
+        "Boston (MA)",
+        "Los Angeles (CA)",
+        "San Francisco (CA)",
+        "Seattle (WA)",
+        "Atlanta (GA)",
+        "New York City (NY)",
+        "Portland (OR)",
+        "Austin (TX)",
+        "Portland (ME)",
+    ],
+    multiple=True,
 )
 
 
 @render_plotly
 def sales_over_time():
     df = dat()
-    sales = df.groupby(['city', 'month'])['quantity_ordered'].sum().reset_index()
+    sales = df.groupby(["city", "month"])["quantity_ordered"].sum().reset_index()
     months_orders = calendar.month_name[1:]
-    fig = px.bar(sales, x = 'month', y = 'quantity_ordered', color = 'city',category_orders={'month': months_orders}, title = 'Sales over time by city')
+    fig = px.bar(
+        sales,
+        x="month",
+        y="quantity_ordered",
+        color="city",
+        category_orders={"month": months_orders},
+        title="Sales over time by city",
+    )
     return fig
 
 
 with ui.card():
     ui.card_header("Sample Sales Data")
+
     @render.data_frame
     def sample_sales_data():
         return dat().head(10)
