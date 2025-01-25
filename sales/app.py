e1d53f33e94b1287cf6c52437baa2674f8bc959e
Prince Mfinanga <noahprince2@icloud.com>
2025-01-25 19:56:18 +0300

    Implemented some basic stuffs

--- sales/app.py
+++ sales/app.py
@@ -8,9 +8,15 @@ from shiny import reactive
 from shiny.express import render, input, ui
 from shinywidgets import render_plotly
 
-ui.page_opts(title="Sales Dashboard - video 1 of 5", fillable=True)
+ui.page_opts(title="Sales Dashboard", fillable=False)
 
 ui.input_numeric("n", label="Number of products to show", value=5, min=0, max=20)
+
+ui.input_checkbox("bar_color", "Make Bars Red?", False)
+
+@reactive.calc
+def color():
+    return "red" if input.bar_color() else "blue"
 
 
 @reactive.calc
@@ -22,11 +28,11 @@ def dat():
     return df
 
 
-# @render_plotly
-# def plot1():
-#     df = dat()
-#     top_sales = df.groupby("product")['quantity_ordered'].sum().nlargest(input.n()).reset_index()
-#     return px.bar(top_sales, x = 'product', y = 'quantity_ordered', title = f'Top {input.n()} products by quantity sold')
+@render_plotly
+def plot1():
+    df = dat()
+    top_sales = df.groupby("product")['quantity_ordered'].sum().nlargest(input.n()).reset_index()
+    return px.bar(top_sales, x = 'product', y = 'quantity_ordered', title = f'Top {input.n()} products by quantity sold')
 
 ui.input_selectize(
     "city",
@@ -44,6 +50,7 @@ ui.input_selectize(
         "Portland (ME)",
     ],
     multiple=True,
+    selected='Boston (MA)'
 )
 
 
@@ -51,15 +58,17 @@ ui.input_selectize(
 def sales_over_time():
     df = dat()
     sales = df.groupby(["city", "month"])["quantity_ordered"].sum().reset_index()
+    sales_by_city = sales[sales["city"].isin(input.city())]
     months_orders = calendar.month_name[1:]
     fig = px.bar(
-        sales,
+        sales_by_city,
         x="month",
         y="quantity_ordered",
         color="city",
+        title= f"Sales over time by city",
         category_orders={"month": months_orders},
-        title="Sales over time by city",
     )
+    #fig.update_traces(marker_color = color())
     return fig
 
 
